4ff7c5ad6b2603250e94f3d0bbdc305314640101
Alexis Claude André Dewaele <alexis.dewaele@me.com>
2016-10-07 10:22:16 +0200

    GUI Sprint#1 Option menu
    This commit adds an option menu to the app. It is just a visual
    component (i.e. no actions have been linked to the menu)

--- app/src/main/java/ch/epfl/sweng/project/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/project/MainActivity.java
@@ -2,6 +2,8 @@ package ch.epfl.sweng.project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 /**
  * Your app's main activity.
@@ -9,13 +11,18 @@ import android.os.Bundle;
 public final class MainActivity extends AppCompatActivity {
     // TODO replace this code with your app code!
 
-    public static int add(final int a, final int b) {
-        return a + b;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options, menu);
+        return true;
+    }
+
 }
