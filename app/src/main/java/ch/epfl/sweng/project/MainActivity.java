8a349a2e3310219e69153ccfa70017009ee575d4
olic0 <olivier.couque@live.fr>
2016-10-07 16:45:18 +0200

    Included picture capture and permissions to do so

--- app/src/main/java/ch/epfl/sweng/project/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/project/MainActivity.java
@@ -1,7 +1,9 @@
 package ch.epfl.sweng.project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Your app's main activity.
@@ -9,13 +11,14 @@ import android.os.Bundle;
 public final class MainActivity extends AppCompatActivity {
     // TODO replace this code with your app code!
 
-    public static int add(final int a, final int b) {
-        return a + b;
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void send(View v){
+        Intent pictureIntent = new Intent(this, PictureActivity.class);
+        startActivity(pictureIntent);
     }
 }
