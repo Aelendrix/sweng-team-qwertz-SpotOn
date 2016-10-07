c748b1fb05a854cf24f8d8074da4bb86fe806dfb
Alexis Claude André Dewaele <alexis.dewaele@me.com>
2016-10-07 15:18:07 +0200

    This commit adds parts of the GUI i.e. it has a settings menu which doesn't do anything yet but which is here. An action bar is created but it doesn't have tabs with swipe views yet.

--- app/src/main/java/ch/epfl/sweng/project/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/project/MainActivity.java
@@ -1,7 +1,10 @@
 package ch.epfl.sweng.project;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 
@@ -10,12 +13,34 @@ import android.view.MenuInflater;
  */
 public final class MainActivity extends AppCompatActivity {
     // TODO replace this code with your app code!
-
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+/*
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        */
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new MyStoriesFragment(), "My Stories");
+        adapter.addFragment(new CameraFragment(), "Camera");
+        adapter.addFragment(new StoriesAroundMeFragment(), "Stories around me");
+        viewPager.setAdapter(adapter);
     }
 
     @Override
