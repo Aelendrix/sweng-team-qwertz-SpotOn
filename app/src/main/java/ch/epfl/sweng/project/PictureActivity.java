cb87569b3b75611fba94df8a7dc6c03687698f7f
olic0 <olivier.couque@live.fr>
2016-10-09 19:50:20 +0200

    Included picture capture and permissions to do so

--- app/src/main/java/ch/epfl/sweng/project/PictureActivity.java
+++ app/src/main/java/ch/epfl/sweng/project/PictureActivity.java
@@ -1,21 +1,16 @@
 package ch.epfl.sweng.project;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
-
-import static android.R.attr.data;
 
 /**
  * Activity that will allow the user to access the camera and take a picture to integrate
@@ -30,7 +25,6 @@ public class PictureActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture);
-        Button click = (Button) findViewById(R.id.captureButton);
         pic = (ImageView) findViewById(R.id.image_view);
 
     }
@@ -38,9 +32,8 @@ public class PictureActivity extends AppCompatActivity {
     /**
      * Method that checks if the app has the permission to use the camera
      * if not, it asks the permission to use it, else it calls the method invokeCamera()
-     * @param view
      */
-    public void dispatchTakePictureIntent(View view){
+    public void dispatchTakePictureIntent(View v){
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
             invokeCamera();
         } else {
