7483246030cd0fdc029df181008401aa13c635d5
Alexis Claude André Dewaele <alexis.dewaele@me.com>
2016-10-10 14:00:32 +0200

    Adds a new Activity for the tabs. Now the MainActivity is the home screen when users launches app. Adds a "Next" button which allows user to go to TabActivity.

--- app/src/main/java/ch/epfl/sweng/project/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/project/MainActivity.java
@@ -1,5 +1,6 @@
 package ch.epfl.sweng.project;
 
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -7,47 +8,31 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 
 /**
  * Your app's main activity.
  */
 public final class MainActivity extends AppCompatActivity {
     // TODO replace this code with your app code!
-    private Toolbar toolbar;
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-/*
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        viewPager = (ViewPager) findViewById(R.id.viewpager);
-        setupViewPager(viewPager);
-
-        tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(viewPager);
-        */
-    }
-
-    private void setupViewPager(ViewPager viewPager) {
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new MyStoriesFragment(), "My Stories");
-        adapter.addFragment(new CameraFragment(), "Camera");
-        adapter.addFragment(new StoriesAroundMeFragment(), "Stories around me");
-        viewPager.setAdapter(adapter);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.options, menu);
         return true;
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, TabActivity.class);
+        startActivity(intent);
     }
 
 }
---
+++ app/src/main/java/ch/epfl/sweng/project/TabActivity.java
@@ -0,0 +1,51 @@
+package ch.epfl.sweng.project;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class TabActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab);
+
+        /*
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        */
+
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new MyStoriesFragment(), "My Stories");
+        adapter.addFragment(new CameraFragment(), "Camera");
+        adapter.addFragment(new StoriesAroundMeFragment(), "Stories around me");
+        viewPager.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options, menu);
+        return true;
+    }
+}
