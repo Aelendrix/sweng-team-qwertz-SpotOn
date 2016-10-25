ef3e1a3d5dd42ff1f77744c4dfb4ddeb4e09630a
sgPepper <quentin.bouvet@hotmail.fr>
2016-10-25 12:42:57 +0200

    implemented pseudocode for FUllSizeImageViewActivity (still needs access to Loacaldatabase, which will be soon on master)

--- app/src/main/java/ch/epfl/sweng/project/FullsizeImageViewActivity.java
+++ app/src/main/java/ch/epfl/sweng/project/FullsizeImageViewActivity.java
@@ -1,23 +1,52 @@
 package ch.epfl.sweng.project;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class FullsizeImageViewActivity extends AppCompatActivity {
 
     public final static String WANTED_IMAGE_PICTUREID = "ch.epfl.sweng.teamqwertz.spoton.WANTED_IMAGE_PICTUREID";
+    private final static Bitmap IMAGE_DEFAULT = //TODO
+    private final static Bitmap IMAGE_FAILURE = //TODO
+
+    private PhotoObject mDisplayedMedia = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fullsize_image_view);
 
+        ImageView viewToSet = (ImageView) findViewById(R.id.fullSizeImageView);
+        // TODO - set a default image or thumbnail
+
         Intent displayImageIntent = getIntent();
-        String WantedImagePictureId = displayImageIntent.getExtras().getString(WANTED_IMAGE_PICTUREID);
+        String wantedImagePictureId = displayImageIntent.getExtras().getString(WANTED_IMAGE_PICTUREID);
 
+        Bitmap imageToDisplay = IMAGE_FAILURE;
+        if(LocalDatabase.hasKey(wantedImagePictureId)){
+            mDisplayedMedia = LocalDatabase.getKey(wantedImagePictureId);
+            imageToDisplay = mDisplayedMedia.getFullSizeImage();
+        }
 
+        viewToSet.setImageBitmap(imageToDisplay);
     }
+
+
+/*    private void recordUpvote(){
+        if(mDisplayedMedia!=null){
+
+        }
+    }
+
+    private void recordDownvote(){
+        if(mDisplayedMedia!=null){
+
+        }
+    }*/
 
 
 }
