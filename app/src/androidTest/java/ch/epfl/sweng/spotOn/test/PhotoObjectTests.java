ab65647f6525d82bb5087ece23d7fd04dc357191
sgPepper <quentin.bouvet@hotmail.fr>
2016-10-27 01:08:18 +0200

    Utility class for generating and testing PhotoOBjects + stubs of a class testing PhotoObjects

---
+++ app/src/androidTest/java/ch/epfl/sweng/spotOn/test/PhotoObjectTests.java
@@ -0,0 +1,75 @@
+package ch.epfl.sweng.spotOn.test;
+
+import android.graphics.Bitmap;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOError;
+import java.sql.Timestamp;
+import java.util.List;
+
+import ch.epfl.sweng.spotOn.media.PhotoObject;
+import ch.epfl.sweng.spotOn.test.util.PhotoObjectUtils;
+
+/**
+ *  Created by quentin on 26.10.16.
+ */
+@RunWith(AndroidJUnit4.class)
+public class PhotoObjectTests {
+
+    @Test
+    public void photoOBjectInstantiatesCorrectly(){
+        Bitmap fullSizePic = null;
+        fullSizePic = PhotoObjectUtils.getBitmapFromURL("https://upload.wikimedia.org/wikipedia/commons/4/4e/Ice_Diving_2.jpg");
+        String authorID = "author";
+        String photoName = "photoName";
+        Timestamp createdDate = new Timestamp(9001);
+        double latitude = 10;
+        double longitude = -10;
+        int radius = 1;
+        assertCreatedObjectMatchesInitializationFields(fullSizePic, authorID, photoName, createdDate, latitude, longitude, radius);
+    }
+
+    /*
+    @Test
+    public void photoObjectConversionToDatabaseIsIdentity(){
+        for (PhotoObject po : PhotoObjectUtils.getAllPO()) {
+            po.upload();
+        }
+        DatabaseReference DBref =
+    }
+    */
+
+
+
+
+
+
+
+
+// PRIVATE HELPERS
+
+    private void assertCreatedObjectMatchesInitializationFields(Bitmap fullSizePic, String authorID, String photoName,
+                                                               Timestamp createdDate, double latitude, double longitude, int radius){
+        PhotoObject po = new PhotoObject(fullSizePic, authorID, photoName,
+                createdDate, latitude, longitude, radius);
+        DatabaseReference DBref = FirebaseDatabase.getInstance().getReference("MediaPath");
+        assert (po.getFullSizeImage() == fullSizePic);
+        assert (po.getAuthorId() == authorID);
+        assert (po.getPhotoName() == photoName);
+        assert (po.getCreatedDate() == createdDate);
+        assert (po.getExpireDate().getTime() > createdDate.getTime());
+        assert (po.getLatitude() == latitude);
+        assert (po.getLongitude() == longitude);
+        assert (po.getRadius() == radius);
+        assert (po.getPictureId().length() == DBref.push().getKey().length());
+    }
+
+
+
+}
---
+++ app/src/androidTest/java/ch/epfl/sweng/spotOn/test/util/PhotoObjectUtils.java
@@ -0,0 +1,111 @@
+package ch.epfl.sweng.spotOn.test.util;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.provider.ContactsContract;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import ch.epfl.sweng.spotOn.media.PhotoObject;
+
+
+/** Provides several static methods constructing premade PhotoObjects
+ *  Created by quentin on 26.10.16.
+ */
+
+public class PhotoObjectUtils {
+
+    public final static int NB_PO_AVAILABLE = 3;
+
+
+// USEFUL METHODS ON PHOTOBJECTS
+
+    /* compares the PhotoOBjects field by field for equality */
+    public static boolean areEquals(PhotoObject p1, PhotoObject p2){
+        return p1.getThumbnail().sameAs(p2.getThumbnail()) &&
+                p1.getPictureId() == p2.getPictureId() &&
+                p1.getAuthorId() == p2.getAuthorId() &&
+                p1.getLatitude() == p2.getLatitude() &&
+                p1.getLongitude() == p2.getLongitude() &&
+                p1.getRadius() == p2.getRadius();
+    }
+
+
+
+// FACTORY METHODS TO INSTANCIATE PREMADE PHOTOBJECTS
+
+    public static PhotoObject getRandomPhotoObject(){
+        return getOnePo(new Random().nextInt(NB_PO_AVAILABLE));
+    }
+
+    public static List<PhotoObject> getAllPO(){
+        List<PhotoObject> l = new ArrayList<>(NB_PO_AVAILABLE);
+        int i = 0;
+        while(i<NB_PO_AVAILABLE){
+            l.add(getOnePo(i));
+            i++;
+        }
+        return l;
+    }
+
+    public static PhotoObject getOnePo(int i){
+        if(i>= NB_PO_AVAILABLE){
+            throw new AssertionError();
+        }
+        switch(i){
+            case 0 : return iceDivingPO();
+            case 1 : return germaynDeryckePO();
+            case 2 : return paulVanDykPO();
+            default : throw new AssertionError();
+        }
+    }
+
+
+// PHOTOOBJECTS INDIVIDUAL CONSTRUCTORS
+
+    public static PhotoObject paulVanDykPO(){
+        Bitmap image =  getBitmapFromURL("https://upload.wikimedia.org/wikipedia/commons/8/89/Paul_van_Dyk_DJing.jpg");
+        return new PhotoObject(image, "paul", "cc", new Timestamp(1), 1, 1, 1);
+    }
+
+    public static PhotoObject germaynDeryckePO(){
+        Bitmap image = getBitmapFromURL("https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Germain_Derycke_%281954%29.jpg/450px-Germain_Derycke_%281954%29.jpg");
+        return new PhotoObject(image, "author1", "photo1", new Timestamp(1), 1, 1, 1);
+    }
+
+    public static PhotoObject iceDivingPO(){
+        Bitmap image = getBitmapFromURL("https://upload.wikimedia.org/wikipedia/commons/4/4e/Ice_Diving_2.jpg");
+        return new PhotoObject(image, "saruman", "icediving", new Timestamp(1), 1, 1, 1);
+    }
+
+
+
+
+// HELPERS
+
+    /* Retrieves a bitmap file from the internet since it's the easiest way to get one consistently across multiple computers */
+    public static Bitmap getBitmapFromURL(String src){
+        try {
+            URL url = new URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            return myBitmap;
+        } catch (IOException e) {
+            throw new Error("Couldn't fetch image from the internet");
+        }
+    }
+
+}
