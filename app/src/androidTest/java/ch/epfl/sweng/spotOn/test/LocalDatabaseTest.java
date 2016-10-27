f758bb2658d055fc64de88e557b5d0263d3cb735
aelendrix <nicolas.brunner@epfl.ch>
2016-10-27 15:47:42 +0200

    test photoObject

--- app/src/androidTest/java/ch/epfl/sweng/spotOn/test/LocalDatabaseTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/spotOn/test/LocalDatabaseTest.java
@@ -8,10 +8,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import ch.epfl.sweng.spotOn.localObjects.LocalDatabase;
 import ch.epfl.sweng.spotOn.media.PhotoObject;
 import ch.epfl.sweng.spotOn.test.util.PhotoObjectUtils;
+import ch.epfl.sweng.spotOn.util.Pair;
 
 /**
  * Created by nico on 27.10.16.
@@ -19,7 +21,9 @@ import ch.epfl.sweng.spotOn.test.util.PhotoObjectUtils;
 @RunWith(AndroidJUnit4.class)
 public class LocalDatabaseTest {
 
-    PhotoObject photo1 = PhotoObjectUtils.getRandomPhotoObject();
+    PhotoObject photo1 = PhotoObjectUtils.paulVanDykPO();
+    PhotoObject photo2 = PhotoObjectUtils.iceDivingPO();
+    PhotoObject photo3 = PhotoObjectUtils.germaynDeryckePO();
     Location location = new Location("");
 
 
@@ -65,4 +69,21 @@ public class LocalDatabaseTest {
             throw new AssertionError("Location not correctly set");
         }
     }
+
+    @Test
+    public void getThumbnailListTest() throws Exception{
+        LocalDatabase.clearData();
+        LocalDatabase.addPhotoObject(photo1);
+        LocalDatabase.addPhotoObject(photo2);
+        LocalDatabase.addPhotoObject(photo3);
+        List<Pair<Bitmap,String>> thumbList = LocalDatabase.getThumbnailArray();
+        if(thumbList.size()!=3)
+        {
+            throw new AssertionError("return a list with a different size than the map");
+        }
+        //TODO: finish this test, i blame @sg.pepper to use a list of pair instead of map
+        //if(thumbList.
+    }
+
+
 }
