8497f81210d60604f95ec7c3ca48f6fae2bc6c08
Alexis Claude André Dewaele <alexis.dewaele@me.com>
2016-11-03 16:43:35 +0100

    Finishes the CameraTest.

--- app/src/androidTest/java/ch/epfl/sweng/spotOn/test/gui/CameraTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/spotOn/test/gui/CameraTest.java
@@ -4,32 +4,22 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.Intents.intending;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static ch.epfl.sweng.spotOn.test.gui.ImageViewCatcher.hasDrawable;
 import static org.hamcrest.Matchers.not;
 
 import android.app.Activity;
-import android.app.Instrumentation;
 import android.app.Instrumentation.ActivityResult;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.intent.Intents;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.filters.LargeTest;
-import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.support.v7.app.ActionBar;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -45,12 +35,12 @@ import ch.epfl.sweng.spotOn.gui.TabActivity;
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class CameraTest {
-    /*
+
     @Rule
     public IntentsTestRule<TabActivity> intentsRule = new IntentsTestRule<>(TabActivity.class);
 
     @Before
-    public void stubCameraIntent(){
+    public void stubCameraIntent() {
         ActivityResult result = createImageCaptureStub();
         intending(hasAction(MediaStore.ACTION_IMAGE_CAPTURE)).respondWith(result);
     }
@@ -62,47 +52,16 @@ public class CameraTest {
 
         onView(withId(R.id.captureButton)).perform(click());
 
-        //onView(withId(R.id.image_view)).check(matches(hasDrawable()));
-        Intents.release();
+        onView(withId(R.id.image_view)).check(matches(hasDrawable()));
+
     }
 
-    private ActivityResult createImageCaptureStub(){
+    private ActivityResult createImageCaptureStub() {
         Bundle bundle = new Bundle();
-        Bitmap icon = BitmapFactory.decodeResource(
-                InstrumentationRegistry.getTargetContext().getResources(),
-                R.mipmap.ic_launcher);
+        bundle.putParcelable("data", BitmapFactory.decodeResource(intentsRule.getActivity().getResources(), R.mipmap.ic_launcher));
+
         Intent resultData = new Intent();
-        resultData.putExtra("data", icon);
+        resultData.putExtras(bundle);
         return new ActivityResult(Activity.RESULT_OK, resultData);
-    }
-    */
-/*
-    @Test
-    public void takePictureTest() {
-        onView(withId(R.id.viewpager)).perform(swipeLeft());
-        Bitmap icon = BitmapFactory.decodeResource(InstrumentationRegistry.getTargetContext().getResources(), R.mipmap.ic_launcher);
-
-        Intent resultData = new Intent();
-        resultData.putExtra("data", icon);
-        ActivityResult result = new ActivityResult(Activity.RESULT_OK, resultData);
-        intending(hasAction(MediaStore.ACTION_IMAGE_CAPTURE)).respondWith(result);
-        onView(withId(R.id.captureButton)).perform(click());
-        intended(hasAction(MediaStore.ACTION_IMAGE_CAPTURE));
-    }
-*/
-
-    @Rule
-    public IntentsTestRule<TabActivity> mRule = new IntentsTestRule<>(TabActivity.class);
-
-    @Test
-    public void captureTest() {
-        onView(withText("Camera")).perform(click());
-        Bitmap icon = BitmapFactory.decodeResource(InstrumentationRegistry.getTargetContext().getResources(), R.mipmap.ic_launcher);
-        Intent resultData = new Intent();
-        resultData.putExtra("data", icon);
-        ActivityResult result = new ActivityResult(Activity.RESULT_OK, resultData);
-        intending(toPackage("com.android.camera")).respondWith(result);
-        onView(withId(R.id.captureButton)).perform(click());
-        
     }
 }
