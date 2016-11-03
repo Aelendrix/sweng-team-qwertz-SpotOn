1fdeaf6cc8c629e76b7b99ebda1ed7672d205144
Alexis Claude André Dewaele <alexis.dewaele@me.com>
2016-11-03 16:54:59 +0100

    Before merging master into local branch

--- app/src/androidTest/java/ch/epfl/sweng/spotOn/test/gui/TabActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/spotOn/test/gui/TabActivityTest.java
@@ -1,23 +1,12 @@
 package ch.epfl.sweng.spotOn.test.gui;
 
 
-import android.app.Activity;
-import android.app.Instrumentation;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.support.design.widget.TabLayout;
 import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.Espresso;
-import android.support.test.espresso.IdlingResource;
 import android.support.test.espresso.intent.Intents;
-import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.filters.SmallTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.support.v7.app.ActionBar;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -28,9 +17,7 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.action.ViewActions.swipeRight;
 import static android.support.test.espresso.intent.Intents.intended;
-import static android.support.test.espresso.intent.Intents.intending;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
@@ -50,45 +37,6 @@ import ch.epfl.sweng.spotOn.gui.TabActivity;
 @SmallTest
 public class TabActivityTest {
 
-    public IdlingResource startTiming(long time) {
-        IdlingResource idlingResource = new ElapsedTimeIdlingResource(time);
-        Espresso.registerIdlingResources(idlingResource);
-        return idlingResource;
-    }
-    public void stopTiming(IdlingResource idlingResource) {
-        Espresso.unregisterIdlingResources(idlingResource);
-    }
-    public class ElapsedTimeIdlingResource implements IdlingResource {
-        private long startTime;
-        private final long waitingTime;
-        private ResourceCallback resourceCallback;
-
-        public ElapsedTimeIdlingResource(long waitingTime) {
-            this.startTime = System.currentTimeMillis();
-            this.waitingTime = waitingTime;
-        }
-
-        @Override
-        public String getName() {
-            return ElapsedTimeIdlingResource.class.getName() + ":" + waitingTime;
-        }
-
-        @Override
-        public boolean isIdleNow() {
-            long elapsed = System.currentTimeMillis() - startTime;
-            boolean idle = (elapsed >= waitingTime);
-            if (idle) {
-                resourceCallback.onTransitionToIdle();
-            }
-            return idle;
-        }
-
-        @Override
-        public void registerIdleTransitionCallback(ResourceCallback resourceCallback) {
-            this.resourceCallback = resourceCallback;
-        }
-    }
-
     @Rule
     public ActivityTestRule<TabActivity> mActivityTestRule = new ActivityTestRule<>(TabActivity.class);
 
@@ -96,8 +44,6 @@ public class TabActivityTest {
     public void swipe_between_fragments(){
         onView(withId(R.id.viewpager)).perform(swipeLeft());
         onView(withId(R.id.viewpager)).perform(swipeLeft());
-       //IdlingResource idlingResource = startTiming(5000);
-        //stopTiming(idlingResource);
         onView(withText("Camera")).perform(click());
         onView(withId(R.id.viewpager)).perform(swipeRight());
     }
