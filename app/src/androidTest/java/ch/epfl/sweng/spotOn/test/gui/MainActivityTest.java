ba3f1f3c30fcfca9ce7d059624380171dc73c48b
aelendrix <nicolas.brunner@epfl.ch>
2016-11-09 16:23:57 +0100

    test main activity

---
+++ app/src/androidTest/java/ch/epfl/sweng/spotOn/test/gui/MainActivityTest.java
@@ -0,0 +1,31 @@
+package ch.epfl.sweng.spotOn.test.gui;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import ch.epfl.sweng.spotOn.R;
+import ch.epfl.sweng.spotOn.gui.MainActivity;
+import ch.epfl.sweng.spotOn.gui.ViewFullsizeImageActivity;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void launchFullPictureActivity() throws Exception{
+        Thread.sleep(2000);
+        onView(withId(R.id.mainLoginButton)).perform(click());
+        Thread.sleep(2000);
+
+    }
+
+}
