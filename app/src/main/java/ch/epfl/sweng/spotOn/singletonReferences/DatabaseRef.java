6d4f41b22fb17ff4d4a61223df1b447ac8ae3ee2
sgPepper <quentin.bouvet@hotmail.fr>
2016-11-22 09:17:59 +0100

    renamed database provider to comply with tests

--- app/src/main/java/ch/epfl/sweng/spotOn/singletonReferences/DatabaseRef.java
+++ app/src/main/java/ch/epfl/sweng/spotOn/singletonReferences/DatabaseRef.java
@@ -12,7 +12,7 @@ import com.google.maps.android.BuildConfig;
 
 public class DatabaseRef{
 
-    private final static String mMediaDirectoryString = "MediaDirectory_quentin";
+    private final static String mMediaDirectoryString = "MediaDirectory";
     private final static DatabaseReference mMediaDirectory = FirebaseDatabase.getInstance().getReference(mMediaDirectoryString);
 
     private final static String mUsersDirectoryString = "UsersDirectory";
@@ -44,8 +44,7 @@ public class DatabaseRef{
         if(pictureID == null)
         {
             throw new IllegalArgumentException("Error in DatabaseRef: deletePhotoObjectFromDB, pictureId is null");
-        }
-        else {
+        } else {
             mMediaDirectory.child(pictureID).removeValue();
         }
     }
