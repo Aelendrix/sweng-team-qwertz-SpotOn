8527a52fac1b7eb5c65ff0657e50de0cd0c2b076
aelendrix <nicolas.brunner@epfl.ch>
2016-11-23 16:00:25 +0100

    .

--- app/src/androidTest/java/ch/epfl/sweng/spotOn/test/ToastProviderMethodTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/spotOn/test/ToastProviderMethodTest.java
@@ -5,6 +5,7 @@ import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -13,6 +14,7 @@ import org.junit.runner.RunWith;
 import ch.epfl.sweng.spotOn.gui.MainActivity;
 import ch.epfl.sweng.spotOn.localObjects.LocalDatabase;
 import ch.epfl.sweng.spotOn.localisation.ConcreteLocationTracker;
+import ch.epfl.sweng.spotOn.singletonReferences.DatabaseRef;
 import ch.epfl.sweng.spotOn.test.util.MockLocationTracker_forTest;
 import ch.epfl.sweng.spotOn.user.User;
 import ch.epfl.sweng.spotOn.utils.ServicesChecker;
@@ -106,6 +108,11 @@ public class ToastProviderMethodTest {
                 }
             }
         });
+    }
+
+    @After
+    public void clear(){
+        DatabaseRef.deleteUserFromDB("cestDoncTonAmi");
     }
 
 }
---
+++ app/src/androidTest/java/ch/epfl/sweng/spotOn/test/fileDeletion/DeleteExpirePhotoTest.java
@@ -0,0 +1,32 @@
+package ch.epfl.sweng.spotOn.test.fileDeletion;
+
+import android.support.annotation.NonNull;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+
+import ch.epfl.sweng.spotOn.media.PhotoObject;
+import ch.epfl.sweng.spotOn.test.util.PhotoObjectTestUtils;
+
+/**
+ * Created by nico on 23.11.16.
+ */
+@RunWith(AndroidJUnit4.class)
+public class DeleteExpirePhotoTest {
+
+    String pictureID;
+    @Before
+    public void initLocalDatabase(){
+        PhotoObject po = PhotoObjectTestUtils.veryOldTimestampPicture();
+        pictureID = po.getPictureId();
+        po.upload(true, new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {}
+        });
+
+    }
+}
