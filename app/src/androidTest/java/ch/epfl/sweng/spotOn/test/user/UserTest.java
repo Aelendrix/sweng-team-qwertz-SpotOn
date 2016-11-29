a33ca6c59c3ab7ff68d1efb5c3d422b9b130d733
sgPepper <quentin.bouvet@hotmail.fr>
2016-11-29 14:34:24 +0100

    added back user tests

--- app/src/androidTest/java/ch/epfl/sweng/spotOn/test/user/UserTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/spotOn/test/user/UserTest.java
@@ -1,49 +1,49 @@
-// MLB this needs fixing - randomly fails
-//
-// package ch.epfl.sweng.spotOn.test.user;
-//
-//import android.support.test.runner.AndroidJUnit4;
-//
-//
-//import org.junit.After;
-//import org.junit.Assert;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//
-//import ch.epfl.sweng.spotOn.singletonReferences.DatabaseRef;
-//import ch.epfl.sweng.spotOn.user.User;
-//
-//@RunWith(AndroidJUnit4.class)
-//public class UserTest {
-//
-//    private User testUser = null;
-//
-//    @After
-//    public void removeTestUser() {
-//        DatabaseRef.deleteUserFromDB(testUser.getUserId());
-//        testUser.destroy();
-//    }
-//
-//
-//    @Test
-//    public void testSetAndGetUser(){
-//        User.initializeFromFb("firstname", "lastname", "mlb");
-//        testUser = User.getInstance();
-//        testUser.setKarma(500);
-//        testUser.setRemainingPhotos(User.computeMaxPhotoInDay(testUser.getKarma()));
-//
-//        try {
-//            Thread.sleep(3000);
-//        } catch (InterruptedException e) {
-//            System.err.print(e);
-//        }
-//
-//        Assert.assertEquals(testUser.getFirstName(), "firstname");
-//        Assert.assertEquals(testUser.getLastName(), "lastname");
-//        Assert.assertEquals(testUser.getUserId(), "mlb");
-//        Assert.assertEquals(testUser.getKarma(), 500);
-//        Assert.assertEquals(testUser.getRemainingPhotos(), User.computeMaxPhotoInDay(500));
-//        Assert.assertEquals(testUser.getIsRetrievedFromDB(), false);
-//        Assert.assertEquals(User.hasInstance(), true);
-//    }
-//}
+ MLB this needs fixing - randomly fails
+
+ package ch.epfl.sweng.spotOn.test.user;
+
+import android.support.test.runner.AndroidJUnit4;
+
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import ch.epfl.sweng.spotOn.singletonReferences.DatabaseRef;
+import ch.epfl.sweng.spotOn.user.User;
+
+@RunWith(AndroidJUnit4.class)
+public class UserTest {
+
+    private User testUser = null;
+
+    @After
+    public void removeTestUser() {
+        DatabaseRef.deleteUserFromDB(testUser.getUserId());
+        testUser.destroy();
+    }
+
+
+    @Test
+    public void testSetAndGetUser(){
+        User.initializeFromFb("firstname", "lastname", "mlb");
+        testUser = User.getInstance();
+        testUser.setKarma(500);
+        testUser.setRemainingPhotos(User.computeMaxPhotoInDay(testUser.getKarma()));
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            System.err.print(e);
+        }
+
+        Assert.assertEquals(testUser.getFirstName(), "firstname");
+        Assert.assertEquals(testUser.getLastName(), "lastname");
+        Assert.assertEquals(testUser.getUserId(), "mlb");
+        Assert.assertEquals(testUser.getKarma(), 500);
+        Assert.assertEquals(testUser.getRemainingPhotos(), User.computeMaxPhotoInDay(500));
+        Assert.assertEquals(testUser.getIsRetrievedFromDB(), false);
+        Assert.assertEquals(User.hasInstance(), true);
+    }
+}
