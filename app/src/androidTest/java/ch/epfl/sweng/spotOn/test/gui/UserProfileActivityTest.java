75f6760de6e9ff35c491d9a2ef3d8f4d1922f874
Marie-LaureBarbier <marielaure2409@gmail.com>
2016-12-02 14:28:16 +0100

    Add test for hardware back button pressed in ViewUserPhotoActivity

--- app/src/androidTest/java/ch/epfl/sweng/spotOn/test/gui/UserProfileActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/spotOn/test/gui/UserProfileActivityTest.java
@@ -1,6 +1,7 @@
 package ch.epfl.sweng.spotOn.test.gui;
 
 
+import android.support.test.espresso.Espresso;
 import android.support.test.espresso.ViewAction;
 import android.support.test.espresso.action.CoordinatesProvider;
 import android.support.test.espresso.action.GeneralClickAction;
@@ -76,6 +77,7 @@ public class UserProfileActivityTest {
         Intents.init();
         onView(withId(R.id.profilePicturesListView)).perform(clickXY(100,40));
         intended(hasComponent(ViewUserPhotoActivity.class.getName()));
+        Espresso.pressBack();
         Intents.release();
     }
 
