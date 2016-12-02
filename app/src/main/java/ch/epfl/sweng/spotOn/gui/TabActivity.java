4d9390725faa0e1e8644e461b0b38e93e897dcd0
Alexis Claude André Dewaele <alexis.dewaele@me.com>
2016-12-02 14:31:56 +0100

    Add text in the about activity and change function of the harware back button

--- app/src/main/java/ch/epfl/sweng/spotOn/gui/TabActivity.java
+++ app/src/main/java/ch/epfl/sweng/spotOn/gui/TabActivity.java
@@ -70,7 +70,10 @@ public class TabActivity extends AppCompatActivity{
      */
     @Override
     public void onBackPressed() {
-        ToastProvider.printOverCurrent("THERE IS NO ESCAPE !", Toast.LENGTH_SHORT);
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(startMain);
     }
 
     public void dispatchTakePictureIntent(View view) {
