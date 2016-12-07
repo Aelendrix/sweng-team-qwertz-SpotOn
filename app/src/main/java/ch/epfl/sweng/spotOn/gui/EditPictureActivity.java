87d70d08f41deb784f4ed728b45cb2f46e250f9f
aelendrix <nicolas.brunner@epfl.ch>
2016-12-07 19:00:37 +0100

    text is now under the finger

--- app/src/main/java/ch/epfl/sweng/spotOn/gui/EditPictureActivity.java
+++ app/src/main/java/ch/epfl/sweng/spotOn/gui/EditPictureActivity.java
@@ -9,6 +9,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.drawable.BitmapDrawable;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
@@ -20,6 +21,7 @@ import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
@@ -46,7 +48,8 @@ public class EditPictureActivity extends AppCompatActivity {
     public String mTextToDraw;
     private Bitmap withoutTextBitmap;
     private boolean textIsDrawn = false;
-
+    private int screenWidth;
+    private int screenHeight;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,6 +62,11 @@ public class EditPictureActivity extends AppCompatActivity {
         mEditedBitmap = TakePictureFragment.getBitmap(mURIofBitmap, this);
         withoutTextBitmap = mEditedBitmap;
         mEditedImageView.setImageBitmap(mEditedBitmap);
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        screenWidth = size.x;
+        screenHeight = size.y;
     }
 
     @Override
@@ -171,7 +179,8 @@ public class EditPictureActivity extends AppCompatActivity {
 
     private boolean drawText(float x, float y) {
         Bitmap addTextBitmap = mEditedBitmap.copy(Bitmap.Config.ARGB_8888, true);
-        Log.d("TextToDraw", mTextToDraw);
+        int imgHeight = addTextBitmap.getHeight();
+        int imgWidth = addTextBitmap.getWidth();
         if(mTextToDraw != null){
             //Edits the bitmap in a canvas
             Log.d("TextToDraw", "entered the loop");
@@ -181,7 +190,7 @@ public class EditPictureActivity extends AppCompatActivity {
             paint.setShadowLayer(10, 0, 0, Color.BLACK);
             paint.setTextSize(50);
             paint.setFakeBoldText(true);
-            canvas.drawText(mTextToDraw, x, y, paint);
+            canvas.drawText(mTextToDraw, x*imgWidth/screenWidth, y*imgHeight/screenHeight, paint);
             mEditedImageView.setImageBitmap(addTextBitmap);
             mEditedBitmap = addTextBitmap;
             return true;
