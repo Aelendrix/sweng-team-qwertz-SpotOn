a71fc11067e786dd7851d16b43373e1f4be03d56
sgPepper <quentin.bouvet@hotmail.fr>
2016-12-08 17:25:08 +0100

    -

--- app/src/androidTest/java/ch/epfl/sweng/spotOn/test/ToastsProviderTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/spotOn/test/ToastsProviderTest.java
@@ -60,13 +60,13 @@ public class ToastsProviderTest {
 
 
 // TESTS
-
     @Test
     public void TestToast1() throws Exception {
         mActivityTestRule.launchActivity(displayFullSizeImageIntent);
+        Thread.sleep(4000);
 
         assertNoDisplayedToast();
-        // single toasts ets displayed for 3.5 seconds
+        // single toasts gets displayed for 3.5 seconds
         ToastProvider.printOverCurrent("baseToast", Toast.LENGTH_LONG);
         Thread.sleep(200);
         //assertSomeDisplayedToast();
@@ -79,6 +79,7 @@ public class ToastsProviderTest {
     @Test
     public void TestToast2() throws Exception {
         mActivityTestRule.launchActivity(displayFullSizeImageIntent);
+        Thread.sleep(4000);
 
         // second toast takes over and extends the duration of baseToast
         ToastProvider.printOverCurrent("baseToast", Toast.LENGTH_LONG);
@@ -95,6 +96,7 @@ public class ToastsProviderTest {
     @Test
     public void TestToast3() throws Exception {
         mActivityTestRule.launchActivity(displayFullSizeImageIntent);
+        Thread.sleep(4000);
 
         // printIfNoCurrent() displays single toast
         ToastProvider.printIfNoCurrent("ToastIfNoCurrent", Toast.LENGTH_LONG);
@@ -103,11 +105,13 @@ public class ToastsProviderTest {
         Thread.sleep(sec5);
         assertNoDisplayedToast();
     }
+
     @Test
     public void TestToast4() throws Exception {
         mActivityTestRule.launchActivity(displayFullSizeImageIntent);
+        Thread.sleep(4000);
 
-        // printIfNoCurrent() shouldn't dispaly toast
+        // printIfNoCurrent() shouldn't display toast
         ToastProvider.printOverCurrent("BaseToast", Toast.LENGTH_LONG);
         Thread.sleep(sec2);
         ToastProvider.printIfNoCurrent("(there should have been no toast after this 'baseToast')",Toast.LENGTH_LONG);
