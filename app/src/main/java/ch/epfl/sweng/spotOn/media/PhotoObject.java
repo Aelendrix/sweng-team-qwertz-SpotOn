e1d6fd2078b14d1be8ce9daa6f33f1fa95cea587
brunowicht <bruno.wicht@epfl.ch>
2016-12-09 09:28:58 +0100

    empty commit

--- app/src/main/java/ch/epfl/sweng/spotOn/media/PhotoObject.java
+++ app/src/main/java/ch/epfl/sweng/spotOn/media/PhotoObject.java
@@ -234,7 +234,7 @@ public class PhotoObject {
     public String processReport(String reporterID){
         String resultProcess  = "";
         if(reporterID.equals(mAuthorID)){
-            resultProcess = "You can't report this picture! please delete it from your profile";
+            resultProcess = "You can't report this picture! please delete it from your profile!";
         }
         else{
             resultProcess = "Thank you for reporting this picture.";
