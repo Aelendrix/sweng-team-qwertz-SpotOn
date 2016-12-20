d032b1111d50ab88730df76db1a7b56efb277869
aelendrix <nicolas.brunner@epfl.ch>
2016-12-20 18:00:33 +0100

    finished the javadoc in firebaseConnectionTracker package

--- app/src/main/java/ch/epfl/sweng/spotOn/FirebaseConnectionTracker/FirebaseConnectionListener.java
+++ app/src/main/java/ch/epfl/sweng/spotOn/FirebaseConnectionTracker/FirebaseConnectionListener.java
@@ -2,11 +2,16 @@ package ch.epfl.sweng.spotOn.FirebaseConnectionTracker;
 
 /**
  * Created by quentin on 07.12.16.
- * 
+ * Interface of a listener of the firebase connection
  */
-
 public interface FirebaseConnectionListener {
 
+    /**
+     * method called when the firebase connection is open
+     */
     void firebaseDatabaseConnected();
+    /**
+     * method called when the firebase connection is closed
+     */
     void firebaseDatabaseDisconnected();
 }
