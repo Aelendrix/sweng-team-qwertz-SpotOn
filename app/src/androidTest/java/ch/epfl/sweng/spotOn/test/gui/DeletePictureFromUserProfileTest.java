398b8a9faf1448fb4bae75e1c5cb6ac7ee9ef7f5
Alexis Dewaele <Alexis@tsf-484-wpa-3-105.epfl.ch>
2016-12-22 19:53:07 +0100

    Corrects Delete picture test

--- app/src/androidTest/java/ch/epfl/sweng/spotOn/test/gui/DeletePictureFromUserProfileTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/spotOn/test/gui/DeletePictureFromUserProfileTest.java
@@ -1,15 +1,10 @@
 package ch.epfl.sweng.spotOn.test.gui;
 
 import android.support.annotation.NonNull;
-import android.support.test.espresso.ViewAction;
-import android.support.test.espresso.action.CoordinatesProvider;
-import android.support.test.espresso.action.GeneralClickAction;
-import android.support.test.espresso.action.Press;
-import android.support.test.espresso.action.Tap;
 import android.support.test.espresso.intent.Intents;
+import android.support.test.filters.SmallTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.view.View;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -37,18 +32,16 @@ import ch.epfl.sweng.spotOn.utils.ServicesChecker;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
- * created by Marie-Laure
- * In the userProfileActivity, test if you can view the full size image and the back button
+ * Created by Alexis on 22/12/2016.
  */
+
 @RunWith(AndroidJUnit4.class)
-public class UserProfileActivityTest {
+public class DeletePictureFromUserProfileTest {
     private PhotoObject po1;
-    private PhotoObject po2;
 
     @Rule
     public ActivityTestRule<UserProfileActivity> mActivityTestRule =
@@ -85,85 +78,28 @@ public class UserProfileActivityTest {
                     HashMap<String, Long> h = new HashMap<>();
                     h.put(po1.getPictureId(), po1.getCreatedDate().getTime());
 
-                    //Add 2nd photoObject
-                    final Object lock2 = new Object();
-                    po2 = PhotoObjectTestUtils.iceDivingPO();
-                    po2.upload(true, new OnCompleteListener() {
-                        @Override
-                        public void onComplete(@NonNull Task task) {
-                            if(task.getException()!=null){
-                                lock2.notify();
-                                throw new IOError(new IOException("LocalDatabaseTestUtils : ERROR - uploading first testPhotoObject failed"));
-                            }else{
-                                synchronized (lock2){
-                                    lock2.notify();
-                                }
-                            }
-                        }
-                    });
-                    try {
-                        synchronized (lock2) {
-                            lock2.wait();
-                        }
-                    }
-                    catch(InterruptedException e)
-                    {
-                        throw new AssertionError("Interrupted Exception");
-                    }
-                    h.put(po2.getPictureId(), po2.getCreatedDate().getTime());
+
 
                     User mockUser = new MockUser_forTests("julius","caius","Test", 1000, h, true, true);
                     TestInitUtils.initContextMockUser(mockUser);
                     ServicesChecker.getInstance().allowDisplayingToasts(false);
                     LocalDatabase.getInstance().addPhotoObject(po1);
-                    LocalDatabase.getInstance().addPhotoObject(po2);
                 }
             };
 
     @Test
-    public void testPressBackButton(){
+    public void deletePictureUserProfileActivity() {
         Intents.init();
-        final UserProfileActivity userProfileActivity = mActivityTestRule.getActivity();
-        userProfileActivity.runOnUiThread(new Runnable() {
-            public void run() {
-                userProfileActivity.onBackPressed();
-            }
-        });
-        Intents.release();
+        onView(withId(R.id.deletePictureButton)).perform(click());
+        onView(withText("Cancel")).perform(click());
+        onView(withId(R.id.deletePictureButton)).perform(click());
+        onView(withText("Delete")).perform(click());
     }
 
-    @Test
-    public void startViewUserPhotoActivity(){
-        Intents.init();
-        onView(withId(R.id.profilePicturesListView)).perform(clickXY(100,40));
-        //intended(hasComponent(ViewUserPhotoActivity.class.getName()));
-        //this need to be changed, because the pressBack occurred before going into the FullSizeImage view
-        //Espresso.pressBack();
-        Intents.release();
-    }
-
-    public ViewAction clickXY(final int x, final int y){
-        return new GeneralClickAction(
-                Tap.SINGLE,
-                new CoordinatesProvider() {
-                    @Override
-                    public float[] calculateCoordinates(View view) {
-                        final int[] screenPos = new int[2];
-                        view.getLocationOnScreen(screenPos);
-                        final float screenX = screenPos[0] + x;
-                        final float screenY = screenPos[1] + y;
-                        return new float[] {screenX, screenY};
-                    }
-                },
-                Press.FINGER);
-    }
 
     @After
-    public void clearObject(){
+    public void clearObject() {
         DatabaseRef.deletePhotoObjectFromDB(po1.getPictureId());
         StorageRef.deletePictureFromStorage(po1.getPictureId());
-
-        DatabaseRef.deletePhotoObjectFromDB(po2.getPictureId());
-        StorageRef.deletePictureFromStorage(po2.getPictureId());
     }
 }
